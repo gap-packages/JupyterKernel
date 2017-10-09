e747ccd2917d35ae867f10e9bd5209ddda312d22
Markus Pfeiffer <markus.pfeiffer@morphism.de>
2017-10-09 13:15:25 +0100

    Some documentation updates

--- gap/JupyterUtil.gd
+++ gap/JupyterUtil.gd
@@ -1,5 +1,10 @@
 
-
+#! @Chapter Jupyter Utility Functions
+#! This chapter gives reference to the functions that are 
+#! invoked by the Jupyter kernel to handle requests from the
+#! jupyter frontend.
+#! 
+#! @Section Handlers for Jupyter requests
 #! @Description
 #!   Method that provides rich viewing experience if
 #!   code used inside Jupyter
@@ -10,15 +15,26 @@ DeclareOperation("JUPYTER_ViewString", [IsObject]);
 DeclareGlobalFunction("JUPYTER_print");
 
 #! @Description
-#!   Jupyter completion
+#!   This function is called when the user presses Tab in a code
+#!   cell and produces a list of possible completions. It is passed the
+#!   current code in the cell, and the curser position inside the code.
+#!
 DeclareGlobalFunction("JUPYTER_Complete");
 
 #! @Description
-#!   Jupyter inspection
+#!   This function is called when the user presses Shift-Tab in a code
+#!   cell. It tries to extract some documentation to display for brief
+#!   inspection, and the full documentation for full inspection.
+#!
+#!   If it detects an object under the cursor it returns a list of
+#!   known filters and attributes of that object. 
+
 DeclareGlobalFunction("JUPYTER_Inspect");
 
+#! @Section Additional Utility Functions
+#!
 #! @Description
-#!   Current date and time as ISO8601 timestamp
-#!   Don't trust this function
+#!   Current date and time as ISO8601 timestamp.
+#!   Don't trust this function.
 DeclareGlobalFunction("ISO8601Stamp");
 

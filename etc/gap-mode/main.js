1330a859262ea921e9eedfca1f1ca760df0a2e76
Manuel Martins <manuelmartins25@gmail.com>
2018-01-24 17:15:15 +0000

    string matching regex
    includes also builtin function of the francy package

--- etc/gap-mode/main.js
+++ etc/gap-mode/main.js
@@ -1,14 +1,16 @@
 // Notebook Extension to allow GAP Mode on Jupyter
 
+/* global define */
+
 define([
   'base/js/namespace',
   './gap'
-], function (Jupyter) {
-  "use strict";
+], function () {
+  'use strict';
 
   return {
     load_ipython_extension: function () {
-      console.log('Loading GAP Mode...');
+      console.log('Loading GAP Mode for Codemirror...');
     }
   };
 

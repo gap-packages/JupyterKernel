5dad4350255c1c94d672754bd36c0724844220bf
Markus Pfeiffer <markus.pfeiffer@st-andrews.ac.uk>
2018-05-24 15:43:03 +0100

    Add testing for the Jupyter kernel by using nbconvert

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 set -ex
 
-GAP="$GAPROOT/bin/gap.sh --quitonbreak -q"
-
 mkdir $COVDIR
+GAP="$GAPROOT/bin/gap.sh --quitonbreak -q"
 $GAP --cover $COVDIR/test.coverage tst/testall.g
+
+export JUPYTER_GAP_EXECUTABLE="$PWD/$GAPROOT/bin/gap.sh --quitonbreak --cover $PWD/$COVDIR/testnb.coverage"
+jupyter nbconvert tst/test.ipynb --to notebook --execute
+
